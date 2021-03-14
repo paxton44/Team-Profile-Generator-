2e24aec018fc67e7ff1de02978f70ae9da3a8e2f
Mitchell Flood <mitchflood1@gmail.com>
2021-03-14 16:30:27 -0600

    adding index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+//add requirements
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+//consts for each type of employee
+const Employee = require('./library/employee');
+const Engineer = require('./library/engineer');
+const Intern = require('./library/intern');
+const Manager = require('./library/manager');
+
+
+//make questions for team member (use node js mini project as solid example.)
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'companyName',
+      message: "Enter your Company's name",
+    },
+    {
+      type: 'input',
+      name: 'manager',
+      message: "Enter the Manager's ID number.",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Enter the Manager's email address.",
+    },
+    {
+      type: 'input',
+      name: 'phoneManager',
+      message: "Enter the Manager's phone number.",
+    },
+  ]);
